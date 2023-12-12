5239f04ddd6af2ba6bcb9217501379668cccfbbd
Marcin <m.szablak30@gmail.com>
2023-12-12 21:27:09 +0100

    tic tac toe works| to do animations, dark mode

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -3,17 +3,16 @@
 interface Stats {
     X_Wins: number,
     O_Wins: number,
-    ai_Wins: number,
     draw: number
 }
 
-export default{
-    data(){
-        return{
+export default {
+    data() {
+        return {
             board: [
-                ["","",""],
-                ["","",""],
-                ["","",""]
+                ["", "", ""],
+                ["", "", ""],
+                ["", "", ""]
             ] as Array<Array<string>>,
             playerOrder: "X" as string,
             gameOver: false as boolean,
@@ -21,48 +20,49 @@ export default{
             stats: {
                 X_Wins: 0,
                 O_Wins: 0,
-                ai_Wins: 0,
                 draw: 0,
             } as Stats,
+            aiPlay: false as boolean,
         }
     },
-    methods:{
-        playerClick(rowIndex: any, colIndex: any){
-            if(this.board[rowIndex][colIndex] == ""){
+    methods: {
+        playerClick(rowIndex: any, colIndex: any): void {
+            if (this.board[rowIndex][colIndex] == "") {
                 this.board[rowIndex][colIndex] = this.playerOrder
                 this.clicks += 1
-                if (this.checkWinCondition()){
+                if (this.checkWinCondition()) {
                     this.gameOver = true
                     switch (this.playerOrder) {
                         case "X":
                             this.stats.X_Wins += 1
                             break;
-
                         default:
                             this.stats.O_Wins += 1
                             break;
                     }
+                    this.playerOrderChange()
                     return;
-
                 }
                 this.playerOrderChange()
+                if (this.aiPlay) this.aiMove();
             }
-            if (this.clicks >= 9){
+            if (this.clicks >= 9) {
                 this.gameOver = true
                 this.playerOrder = "none"
                 this.stats.draw += 1
             }
             return
         },
-        playerOrderChange(){
-            if(this.playerOrder == "X"){
-                this.playerOrder = "O"
-                return
+        playerOrderChange(): void {
+            if (this.playerOrder === "X" && !this.gameOver) {
+                this.playerOrder = "O";
+                return;
             }
-            this.playerOrder = "X"
-            return
+            this.playerOrder = "X";
+            return;
         },
-        checkWinCondition() {
+        //checks win condytion
+        checkWinCondition(): any {
             const b = this.board;
             const winConditions = [
                 [b[0][0], b[0][1], b[0][2]],
@@ -81,14 +81,69 @@ export default{
                     return a;
                 }
             }
-
             return null;
         },
-        resetGame(){
-            this.gameOver = !this.gameOver
-            this.board = [ ["","",""], ["","",""],["","",""]]
+        //reset game
+        resetGame(): void {
+            this.board = [
+                ["", "", ""],
+                ["", "", ""],
+                ["", "", ""]
+            ]
             this.clicks = 0
             this.playerOrderChange()
+            this.gameOver = !this.gameOver
+        },
+        //make move as ai
+        aiMove(): void {
+            if (!this.gameOver) {
+                let attempts = 0;
+                const maxAttempts = 50;
+
+                while (attempts < maxAttempts) {
+                    const rowIndex = Math.floor(Math.random() * 3);
+                    const colIndex = Math.floor(Math.random() * 3);
+
+                    if (this.board[rowIndex][colIndex] === "") {
+                        this.board[rowIndex][colIndex] = this.playerOrder;
+                        this.clicks += 1;
+
+                        if (this.checkWinCondition()) {
+                            this.gameOver = true;
+                            switch (this.playerOrder) {
+                                case "X":
+                                    this.stats.X_Wins += 1;
+                                    break;
+                                default:
+                                    this.stats.O_Wins += 1;
+                                    break;
+                            }
+                            return;
+                        }
+
+                        this.playerOrderChange();
+                        return;
+                    }
+
+                    attempts++;
+                }
+                this.gameOver = true;
+                this.playerOrder = "none";
+                this.stats.draw += 1;
+            }
+        },
+        //switch second player to ai
+        changeToAi(): void {
+            this.aiPlay = !this.aiPlay
+            this.board = [
+                ["", "", ""],
+                ["", "", ""],
+                ["", "", ""]
+            ]
+            this.clicks = 0
+            this.stats.X_Wins = 0
+            this.stats.O_Wins = 0
+            this.stats.draw = 0
         },
     }
 }
@@ -96,7 +151,9 @@ export default{
 
 <template>
     <div class="main-container unselectable">
+
         <p class="actual-player">{{gameOver && playerOrder === 'none' ? 'Draw' : playerOrder}}</p>
+
         <table class="board">
             <tr
                 class="row"
@@ -106,8 +163,9 @@ export default{
                 <button
                     class="column"
                     :class="{
+
                         'x-theme': board[rowIndex][colIndex] === 'X',
-                        'o-theme': board[rowIndex][colIndex] === 'O'
+                        'o-theme': board[rowIndex][colIndex] === 'O',
                     }"
                     v-for="(col, colIndex) in row"
                     :key="colIndex"
@@ -116,32 +174,51 @@ export default{
                 >{{board[rowIndex][colIndex]}}</button>
             </tr>
         </table>
+
         <div class="stats-box">
-                <p
-                    v-for="(stat, statIndex) in stats"
-                    class="text stats"
-                    :class="{
-                        'x-theme': statIndex.toString() === 'X_Wins',
-                        'o-theme': statIndex.toString() === 'O_Wins'
-                    }"
-                >{{statIndex.toString().replace("_"," ")}}: {{stat}}</p>
-            </div>
+            <p
+                v-for="(stat, statIndex) in stats"
+                class="text stats"
+                :class="{
+                    'x-theme': statIndex.toString() === 'X_Wins',
+                    'o-theme': statIndex.toString() === 'O_Wins',
+                }"
+            >{{statIndex.toString().replace("_"," ")}}: {{stat}}</p>
+        </div>
+
+        <button
+            class="text ai-button"
+            :class="aiPlay ? 'dark': 'light'"
+            v-on:click.prevent="changeToAi"
+        >aiPlay</button>
+
         <div
             :style=" !gameOver ? {display: 'none'}: {display: 'flex'}"
             class="modal"
         >
             <div class="boxModal">
-                <p class="text modalText">{{gameOver && playerOrder !== 'none' ? playerOrder + ' player wins!' : 'Draw'}} </p>
+                <p
+                    class="text modalText"
+                >{{gameOver && playerOrder !== 'none' ? playerOrder + ' player wins!' : 'Draw'}} </p>
                 <button
                     class="text"
                     v-on:click.prevent="resetGame"
                 >Reset</button>
             </div>
         </div>
+
     </div>
 </template>
 
 <style scoped>
+.dark{
+    background-color: black;
+    color: white;
+}
+.light{
+    background-color: whiet;
+    color: black;
+}
 .main-container{
     display: flex;
     flex-direction: column;
@@ -198,6 +275,12 @@ table, tr, td{
     font-family: 'Urbanist', sans-serif;
     font-weight: 900;
     font-size: 100px;
+}
+.column.clicked:focus{
+    animation: pop 0.3s linear 1;
+}
+@keyframes pop{
+  50%  {transform: scale(1.2);}
 }
 .x-theme{
     background-color: var(--new-pink);
